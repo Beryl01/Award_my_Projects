e73325b0b77dae7528f13777403ff69b8cfb58c6
Beryl01 <berylnegesa@gmail.com>
2020-06-08 07:05:41 +0300

    added auth routes

---
+++ rating/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.urls import path, re_path, include
+from django.contrib.auth import views as auth_views
+
+urlpatterns=[
+    path('register',views.register,name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name = 'auth/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'auth/logout.html'),name='logout'),
+   
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
