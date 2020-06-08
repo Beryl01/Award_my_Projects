7207c0c028976af1b6bbb1f970c7dfbd0d969666
Beryl01 <berylnegesa@gmail.com>
2020-06-08 07:07:47 +0300

    added Project Model

---
+++ rating/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.core.validators import URLValidator
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+import datetime as dt
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = 'projects/',default='default.jpg')
+    description = models.TextField(max_length = 500)
+    link = models.TextField(validators=[URLValidator()],null=True)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_all(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def get_project(cls, project_id):
+        project = cls.objects.get(id=site_id)
+        return projects
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects_title = cls.objects.filter(title__icontains=search_term)
+        return sites_title
+
