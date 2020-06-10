369ff5e370f6164d832030449416aa109efb0c0b
Beryl01 <berylnegesa@gmail.com>
2020-06-10 06:33:00 +0300

    fixed my test.py functions

--- rating/tests.py
+++ rating/tests.py
@@ -6,32 +6,41 @@ import datetime as dt
 class ProfileTestClass(TestCase):
     #Set up method
     def setUp(self):
-        self.new_profile =Profile()
+        self.profile=Profile(user="beryl",profile_pic="default.png",bio="award_official",contacts='berybery@gmail.com')
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.new_profile,Profile))
+
     def test_save_method(self):
-        """
-        Function to test that profile is being saved
-        """
         self.profile.save_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles) > 0)
 
+    def test_delete_method(self):
+        self.profile.delete_profile()
+        profiles= Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
 class ProjecTestClass(TestCase):
-    """  
-    Tests Project class and its functions
-    """
     def setUp(self):
-        self.project = Project()
+        self.profile=Profile(user="beryl",profile_pic="default.png",bio="award_official",contacts='berybery@gmail.com')
+        self.project=Project(image="image.png",title="title",description="mypictoday",link='https;//mypictoday',profile=self.profile)
+
     def test_instance(self):
         self.assertTrue(isinstance(self.project, Project))
+
     def test_save_method(self):
-        """
-        Function to test that a project is being saved
-        """
         self.project.save_project()
         projects = Project.objects.all()
-        self.assertTrue(len(projects) > 0)
+        self.assertTrue(len(projects) > 0) 
 
-  
+    def test_search_method(self):
+        self.assertQuerysetEqual(Project.objects.filter(title__icontains='image'), ["<Project: image>"])  
+
+    def test_delete_method(self):
+        self.project.delete_project()
+        projects=Project.objects.all()
+        self.assertTrue(len(projects) == 0) 
+
+           
+
